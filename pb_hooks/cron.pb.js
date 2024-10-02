0bef116c27086bdf80e9a887ec015d730e703feb
Thavirak Svay <macbook.@macbook.local>
2024-10-02 19:09:17 +0700

    warm instance cron

--- pb_hooks/cron.pb.js
+++ pb_hooks/cron.pb.js
@@ -1,6 +1,6 @@
 cronAdd("Generate Prompt Technique", $os.getenv("GENERATE_CRON_SCHEDULE"), async () => {
-  const API_BASE_URL = $os.getenv("API_BASE_URL");
-  const apiUrl = `${API_BASE_URL}/api/prompt-techniques/generate`;
+  const API_BASE_URL = $os.getenv("API_BASE_URL")
+  const apiUrl = `${API_BASE_URL}/api/prompt-techniques/generate`
 
   await $http.send({
     method: "POST",
@@ -9,5 +9,15 @@ cronAdd("Generate Prompt Technique", $os.getenv("GENERATE_CRON_SCHEDULE"), async
       "Content-Type": "application/json",
     },
     body: JSON.stringify({}),
-  });
-});
+  })
+})
+
+cronAdd("Warm instance every 15 minutes", "*/15 * * * *", async () => {
+  const API_BASE_URL = $os.getenv("API_BASE_URL")
+  const apiUrl = `${API_BASE_URL}/api/prompt-techniques`
+
+  await $http.send({
+    method: "GET",
+    url: apiUrl,
+  })
+})
